704008959d07c358b3c658d7956401b01c9ecc9d
GeenDrive <66233204+GeneDrive@users.noreply.github.com>
2022-09-18 17:46:46 +0200

    etwesgt

--- pretpark/KaartItem.cs
+++ pretpark/KaartItem.cs
@@ -2,7 +2,23 @@ namespace Kaart
 {
     public class KaartItem : Tekenbaar
     {
-        private Coordinaat _locatie{get; set;}
+        private Coordinaat _locatie
+        {
+            get
+            {
+                return _locatie;
+            } 
+            set
+            {
+                //if(value.x < 0 || value.y < 0)
+                //{
+                //    throw new Exception("negatief coordinaat mag niet bestaan");
+                //}
+
+// Help
+                _locatie = value;
+            }
+        }
         private Kaart kaart;
         private char Karakter{get;}
 
