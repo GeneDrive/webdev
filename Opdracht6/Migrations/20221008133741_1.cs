e96b500ce1e72b29b2a6abe31ca1e109c6bebb5c
GeenDrive <66233204+GeneDrive@users.noreply.github.com>
2022-10-08 16:11:16 +0200

    setup started

---
+++ Opdracht6/Migrations/20221008133741_1.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Opdracht6.Migrations
+{
+    public partial class _1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Attractie",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Naam = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Attractie", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Attractie");
+        }
+    }
+}
---
+++ Opdracht6/Models/Attractie.cs
@@ -0,0 +1,5 @@
+public class Attractie
+{
+    public int Id { get; set; }
+    public string Naam { get; set; }
+}
